f5fb25809419f05b3bad82809265119fa218551c
Marek Jelinski <markandjel@gmail.com>
2022-02-25 08:06:31 +0100

    Change comments and add model_checkpoint

--- Models/ChatModel.py
+++ Models/ChatModel.py
@@ -3,14 +3,15 @@ import transformers
 
 class ChatModel:
     def __init__(self):
-        # Initialize pipeline
-        self.nlp = transformers.pipeline("conversational", model="microsoft/DialoGPT-medium")
+        # Initialize pipeline with conversational model
+        self.model_checkpoint = "microsoft/DialoGPT-medium"
+        self.nlp = transformers.pipeline("conversational", model=self.model_checkpoint)
 
     def chat(self, text):
-        # Get answer
+        # Get whole answer (conversation ID, User input and bot answer)
         chat = self.nlp(transformers.Conversation(text), pad_token_id=50256)
-        res = str(chat)
-        # Get bot answer
-        res = res[res.find("bot >> ") + 6:].strip()
-        return res
+        response = str(chat)
+        # Get only bot answer
+        response = response[response.find("bot >> ") + 6:].strip()
+        return response
 
