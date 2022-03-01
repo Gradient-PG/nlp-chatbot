1071c0d1871ff7e2e56056c145d391b082bce94c
DominQu <dominik.kuczkowski00@gmail.com>
2022-03-01 22:30:02 +0100

    Improve model downloading process

--- ts/utils/transformer_downloader.py
+++ ts/utils/transformer_downloader.py
@@ -1,10 +1,18 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig
-import torch
+from pathlib import Path
+import os
 
 if __name__ == "__main__":
+    modelpath = Path('ts/models/DialoGPT-medium')
     config = AutoConfig.from_pretrained("microsoft/DialoGPT-medium")
     model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium", config=config)
     tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
-
-    model.save_pretrained(".")
-    tokenizer.save_pretrained(".")
+    try:
+        os.mkdir(modelpath.as_posix())
+    except OSError:
+        print (f"Creation of directory {modelpath.as_posix()} failed")
+        print("Assuming the model is already downloaded")
+    else:
+        print (f"Successfully created directory {modelpath.as_posix()} ")
+        model.save_pretrained(modelpath.as_posix())
+        tokenizer.save_pretrained(modelpath.as_posix())
