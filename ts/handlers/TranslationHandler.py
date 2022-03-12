378e689d8540754e29801f41e14b4bdf030fa728
Marek Jelinski <markandjel@gmail.com>
2022-03-12 20:35:54 +0100

    Add few Logger informations

--- ts/handlers/TranslationHandler.py
+++ ts/handlers/TranslationHandler.py
@@ -15,12 +15,6 @@ class TranslationHandler(BaseHandler, ABC):
     def __init__(self):
         super(TranslationHandler, self).__init__()
         self.initialized = False
-        self._LANG_MAP = {
-            "pl": "Polish",
-            "pol": "Polish",
-            "en": "English",
-            "eng": "English"
-        }
 
     def initialize(self, ctx):
         self.manifest = ctx.manifest
@@ -33,7 +27,6 @@ class TranslationHandler(BaseHandler, ABC):
             if torch.cuda.is_available() and properties.get("gpu_id") is not None
             else "cpu"
         )
-        # self.device = "cpu"
         # read configs for the mode, model_name, etc. from setup_config.json
         setup_config_path = os.path.join(model_dir, "setup_config.json")
         if os.path.isfile(setup_config_path):
@@ -61,27 +54,22 @@ class TranslationHandler(BaseHandler, ABC):
         if text is None:
             text = data[0].get("body")
         sentences = text.decode('utf-8')
-        logger.info("Received text: '%s'", sentences)
+        logger.info(f"Received text is of type {type(sentences)}, sentence: {sentences}")
 
         inputs = self.tokenizer([sentences+self.tokenizer.eos_token], return_tensors="pt")
-        logger.info(f"Encoded input: {inputs}")
+        logger.info(f"Encoded input is of type {type(inputs)}, input: {inputs}")
         return inputs
 
     def inference(self, input_batch):
-        logger.info(f"input_batch type: {type(input_batch)}")
-        # generations = self.model.generate(input_batch.to(self.device))
-        # generations = self.tokenizer.batch_decode(generations, skip_special_tokens=True)
         input_batch.to(self.device)
-        logging.info(f"Input to model {input_batch}")
-        # output = self.model.generate(**input_batch)
+        logging.info(f"Input to model is of type {type(input_batch)}, input: {input_batch}")
         output = self.model.generate(input_ids=input_batch['input_ids'], attention_mask=input_batch['attention_mask'])
-
-        logging.info(f"Output {output}")
-
         return output
 
     def postprocess(self, inference_output):
-        return self.tokenizer.batch_decode(inference_output, skip_special_tokens=True)
+        translated = self.tokenizer.batch_decode(inference_output, skip_special_tokens=True)
+        logger.info(f"Translated sentence is of type {type(translated)}, translated: {translated}")
+        return translated
 
     def handle(self, data, context):
         return super().handle(data, context)
