8e9e2b1aef25ee2a541149c17407a4724ab4adba
Marek Jelinski <markandjel@gmail.com>
2022-03-16 20:06:34 +0100

    change dialo-gpt to blenderbot

--- ts/utils/downloader.py
+++ ts/utils/downloader.py
@@ -1,19 +1,20 @@
 from numpy import dtype
 from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig, MarianMTModel, MarianTokenizer, MarianConfig
+from transformers import BlenderbotSmallForConditionalGeneration, BlenderbotConfig, BlenderbotSmallTokenizer
 from pathlib import Path
 import torch
 import os
 
 if __name__ == "__main__":
-    models = ["DialoGPT-medium", "Helsinki-NLP", "gsarti"]
+    models = ["blenderbot-90M", "Helsinki-NLP", "gsarti"]
     modelpaths = [Path("ts/models/" + models[i]) for i, _ in enumerate(models)]
 
     for i, val in enumerate(models):
         
         if i == 0:
-            config = AutoConfig.from_pretrained("microsoft/" + val)
-            model = AutoModelForCausalLM.from_pretrained("microsoft/" + val, config=config)
-            tokenizer = AutoTokenizer.from_pretrained("microsoft/" + val)
+            config = BlenderbotConfig.from_pretrained("facebook/" + val)
+            model = BlenderbotSmallForConditionalGeneration.from_pretrained("facebook/" + val, config=config)
+            tokenizer = BlenderbotSmallTokenizer.from_pretrained("facebook/" + val)
         elif i == 1:
             config = MarianConfig.from_pretrained("Helsinki-NLP/opus-mt-pl-en")
             model = MarianMTModel.from_pretrained("Helsinki-NLP/opus-mt-pl-en", config=config)
